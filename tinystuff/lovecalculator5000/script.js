34f6b59c98faac28d84b0108cf8c1748cdcbdd35
aisajib <aminulislam333@gmail.com>
2023-09-17 14:13:15 +0400

    Creating love calculator 5000!

---
+++ tinystuff/lovecalculator5000/script.js
@@ -0,0 +1,18 @@
+function calcLove () {
+    // First form's value
+    const person1 = document.getElementById("input1").value;
+
+    // Second form's value
+    const person2 = document.getElementById("input2").value;
+
+    console.log("You are " + person1 + " and your crush is " + person2);
+
+    let percentFinder = Math.random();
+        percentFinder = Math.floor(percentFinder * 100) + 1;
+
+    
+    result = document.querySelector("#result");
+    result.innerHTML = "Hey " + person1 + "! The love percentage for you and " + person2 + " is <span>" + percentFinder + "%</span>!";
+
+}
+
